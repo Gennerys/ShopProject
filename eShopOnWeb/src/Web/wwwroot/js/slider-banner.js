6fb519d3c9f95ffd272ac3caf8f7364fa5ed10f3
Alexander Papirnyk <43519694+Gennerys@users.noreply.github.com>
2019-11-19 11:35:35 +0200

    slider

---
+++ eShopOnWeb/src/Web/wwwroot/js/slider-banner.js
@@ -0,0 +1,48 @@
+﻿$("#banner-carousel").slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	dots: true,
+	arrows: false,
+	//customPaging: function (slider, i) {
+	//	return '<a><img style="width: 10px; height: 10px" src="/wp-content/themes/ShopProm/inc/Urich/assets/active-dot.png" /><img style="width: 10px; height: 10px" src="/wp-content/themes/ShopProm/inc/Urich/assets/active-dot-filled.png" /></a>';
+	//},
+	 //autoplay: true,
+	 //autoplaySpeed: 2000,
+	//prevArrow:
+	//	"<button type='button' class='slick-prev pull-left'><img class='tab-spots-slider-btn' src='/wp-content/themes/ShopProm/inc/Urich/assets/arrow-left.png' alt='#1'/></button>",
+	//nextArrow:
+	//	"<button type='button' class='slick-next pull-right'><img class='tab-spots-slider-btn' src='/wp-content/themes/ShopProm/inc/Urich/assets/arrow-right.png' alt='#2'/></button>",
+	//responsive: [
+	//	{
+	//		breakpoint: 1500,
+	//		settings: {
+	//			slidesToShow: 4,
+	//			slidesToScroll: 4
+	//		}
+	//	},
+	//	{
+	//		breakpoint: 1025,
+	//		settings: {
+	//			slidesToShow: 3,
+	//			slidesToScroll: 3,
+	//			dots: false
+	//		}
+	//	},
+	//	{
+	//		breakpoint: 769,
+	//		settings: {
+	//			slidesToShow: 2,
+	//			slidesToScroll: 2,
+	//			dots: false
+	//		}
+	//	},
+	//	{
+	//		breakpoint: 481,
+	//		settings: {
+	//			slidesToShow: 1,
+	//			slidesToScroll: 1,
+	//			dots: false
+	//		}
+	//	}
+	//]
+});
